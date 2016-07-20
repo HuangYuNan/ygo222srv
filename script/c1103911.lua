e2226349aaafde604796c2684f4d7a79d756ffdd
HuangYuNan <119724848@qq.com>
2016-07-20 21:22:24 +0800

    fix

--- script/c1103911.lua
+++ script/c1103911.lua
@@ -36,8 +36,8 @@ function c1103911.initial_effect(c)
 	e3:SetCategory(CATEGORY_TOHAND)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e3:SetCode(EVENT_ATTACK_ANNOUNCE)
-	e3:SetTarget(c1103911.thtg)
-	e3:SetOperation(c1103911.thop)
+	e3:SetTarget(c1103911.sptg)
+	e3:SetOperation(c1103911.spop)
 	c:RegisterEffect(e3)
 end
 function c1103911.rfilter1(c,tp)
@@ -86,34 +86,22 @@ function c1103911.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
 end
 function c1103911.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_REMOVED) and chkc:IsControler(tp) and c1103911.filter2(chkc,e,tp) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingTarget(c1103911.filter,tp,LOCATION_REMOVED,0,1,nil,e,tp) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectTarget(tp,c1103911.filter,tp,LOCATION_REMOVED,0,1,1,nil,e,tp)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
-end
-function c1103911.spop(e,tp,eg,ep,ev,re,r,rp)
-	if not Duel.IsEnvironment(56433456) then return end
-	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) then
-		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
-	end
-end
-function c1103911.filter1(c)
-	return c:IsFaceup() and c:IsSetCard(0x1240) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
-function c1103911.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(c1103911.filter1,tp,LOCATION_REMOVED,0,1,nil,e,tp) end
+		and Duel.IsExistingMatchingCard(c1103911.filter,tp,LOCATION_REMOVED,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_REMOVED)
 end
-function c1103911.thop(e,tp,eg,ep,ev,re,r,rp)
+function c1103911.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,c1103911.filter1,tp,LOCATION_REMOVED,0,1,1,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,c1103911.filter,tp,LOCATION_REMOVED,0,1,1,nil,e,tp)
 	local tc=g:GetFirst()
 	if tc then
 		Duel.SpecialSummon(tc,SUMMON_TYPE_SPECIAL,tp,tp,false,false,POS_FACEUP)
 	end
+end
+function c1103911.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+
+end
+function c1103911.thop(e,tp,eg,ep,ev,re,r,rp)
+
 end
