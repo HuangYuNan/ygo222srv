81fa22c3725699249b1e145cd70bf523ee662cef
HuangYuNan <119724848@qq.com>
2016-07-31 18:40:10 +0800

    fix

--- script/c19300095.lua
+++ script/c19300095.lua
@@ -10,53 +10,20 @@ function c19300095.initial_effect(c)
 	e1:SetTarget(c19300095.target)
 	e1:SetOperation(c19300095.operation)
 	c:RegisterEffect(e1)
-	if not c19300095.global_check then
-		c19300095.global_check=true
-		local ge1=Effect.CreateEffect(c)
-		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		ge1:SetCode(EVENT_SPSUMMON_SUCCESS)
-		ge1:SetOperation(c19300095.checkop)
-		Duel.RegisterEffect(ge1,0)
-		local ge2=Effect.CreateEffect(c)
-		ge2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		ge2:SetCode(EVENT_PHASE_START+PHASE_DRAW)
-		ge2:SetOperation(c19300095.clear)
-		Duel.RegisterEffect(ge2,0)
-	end
+	Duel.AddCustomActivityCounter(19300095,ACTIVITY_SPSUMMON,c19300095.counterfilter)
 end
-function c19300095.checkop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=eg:GetFirst()
-	while tc do
-		if not tc:IsSetCard(0x193) then
-			c19300095[tc:GetSummonPlayer()]=false
-		end
-		tc=eg:GetNext()
-	end
-end
-function c19300095.clear(e,tp,eg,ep,ev,re,r,rp)
-	c19300095[0]=true
-	c19300095[1]=true
+function c19300095.counterfilter(c)
+	return c:IsSetCard(0x193)
 end
 function c19300095.cffilter(c)
 	return c:IsSetCard(0x193) and c:GetCode()~=19300095 and not c:IsPublic()
 end
 function c19300095.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return c19300095[tp] and Duel.IsExistingMatchingCard(c19300095.cffilter,tp,LOCATION_HAND,0,2,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(c19300095.cffilter,tp,LOCATION_HAND,0,2,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
 	local g=Duel.SelectMatchingCard(tp,c19300095.cffilter,tp,LOCATION_HAND,0,2,2,nil)
 	Duel.ConfirmCards(1-tp,g)
 	Duel.ShuffleHand(tp)
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-	e1:SetTargetRange(1,0)
-	e1:SetTarget(c19300095.splimit)
-	e1:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e1,tp)
-end
-function c19300095.splimit(e,c)
-	return not c:IsSetCard(0x193)
 end
 function c19300095.filter(c)
 	return c:IsSetCard(0x193) and c:IsLevelBelow(3) and c:IsAbleToGrave()
